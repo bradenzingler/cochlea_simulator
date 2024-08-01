8c7093fe31dcf53980c8fa7c09b246ab38497bab
Braden Zingler <brgzingler-i@sbgtv.com>
2024-08-01 16:30:17 -0500

    Clean up code

--- light_controls.py
+++ light_controls.py
@@ -1,52 +1,63 @@
-
-import time
 from rpi_ws281x import *
-import argparse
+import math
 
-# LED strip configuration:
+################################# GLOBALS ######################################
 LED_COUNT      = 144     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
 LED_BRIGHTNESS = 100     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-DELAY          = 1
-
+LED_INVERT     = False   # True to invert the signal 
+LED_CHANNEL    = 0       # Set to '1' for GPIOs 13, 19, 41, 45 or 53
+BM_LENGTH      = 1000    # Length of basiliar membrane (mm)
+K              = 0.88    # Greenwood integration constant
+SMALL_A        = 2.1     # Slope of the straight-line portion of freq-pos curve
+BIG_A          = 165.4   # Scale const between characteristic freq + upper freq
 
 class Lights:
-
     def __init__(self):
         self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         self.strip.begin()
     
+    def pos_to_pixels(self, pos_in_mm):
+        """
+        Map the basiliar membrane position (in mm) to LED strip pixels.
+        """
+        pixels = int((pos_in_mm / BM_LENGTH) * LED_COUNT)
+        return max(0, min(LED_COUNT, pixels))
 
-    def frequency_to_color(self, frequency):
+    def freq_to_pos(self, frequency: float):
+        """
+        Convert frequency to position (mm) using the Greenwood function.
+
+        Reference: Greenwood D. "A cochlear frequency-position for several
+        species - 29 years later", J. Acoust. Soc Am. 1990(87) pp2592-2605.
+        """
+        a = 2.1
+        A = 165.4
+        pos_in_mm = math.log10((frequency/A)+K) * (BM_LENGTH/a)
+        return pos_in_mm
+
+    def frequency_to_strip(self, frequency):
+        """
+        Light up strip based on frequency.
+        """
         self.clear()
-
-        color = Color(0, 0, 0)
-
-        if frequency >= 300 and frequency < 600:
-            color = Color(0, 255, 0)
-
-        elif frequency >= 600 and frequency < 1000:
-            color = Color(250, 234, 3)
-            
-        elif frequency >= 1000 and frequency < 1300:
-            color = Color(255, 0, 0)
-
-        else:
-            color = Color(0, 0, 255)
-
-        self.strip.setPixelColor(self.strip.getPixels(), color)
+        pos = self.freq_to_pos(frequency=frequency)
+        pixels = self.pos_to_pixels(pos_in_mm=pos)
+        for i in (pixels):
+            self.strip.setPixelColor(i, Color(255, 255, 255))
         self.strip.show()
-        
-        # for i in (pixels):
-        #     self.strip.setPixelColor(i, color)
-        #     self.strip.show()
-
     
     def clear(self):
+        """
+        Set all lights to off.
+        """
         for i in range(self.strip.numPixels()):
             self.strip.setPixelColor(i, Color(0, 0, 0))
             self.strip.show()
+
+if __name__ == "__main__":
+    lights = Lights()
+    pos = lights.frequency_to_strip(frequency=1000)
+    print(pos)
--- read_audio.py
+++ read_audio.py
@@ -1,74 +1,77 @@
 #!/usr/bin/env python
 import numpy as np
-from scipy.fftpack import fft, fftfreq
+from scipy.fftpack import fft
 import pyaudio
-import matplotlib.pyplot as plt
 from light_controls import Lights
 
-CHUNK = 1024
-FORMAT = pyaudio.paFloat32
-CHANNELS = 1
-RATE = 44100
-NUM_BINS = 40
+#### AUDIO CONFIG ##############################################################
+CHUNK       = 1024                  # Number of btytes to read in per buffer
+FORMAT      = pyaudio.paFloat32     # Audio format
+CHANNELS    = 1                     # Number of audio channels to record in
+RATE        = 44100                 # Sampling rate
+NUM_BINS    = 1500                  # Bins to split frequency into
 
-# Create audio stream
-p = pyaudio.PyAudio()
-
-def open_stream():
+def open_stream(p):
+    """
+    Open the audio stream.
+    """
     return p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=CHUNK
+        format              = FORMAT,
+        channels            = CHANNELS,
+        rate                = RATE,
+        input               = True,
+        frames_per_buffer   = CHUNK
+        )
+
+def read_audio(stream, frequencies, lights, bin_edges, p):
+    """
+    Read audio input from the opened audio stream
+    """
+    try:
+        while True:
+            data = np.frombuffer(
+                stream.read(CHUNK, 
+                exception_on_overflow=False), 
+                np.float32
+                )
+            yf = fft(data)
+            magnitudes = np.abs(yf[:CHUNK // 2])
+            binned_magnitudes = [np.sum(magnitudes[bin_edges[i]:bin_edges[i + 1]]) for i in range(NUM_BINS)]
+            max_magnitude = np.max(binned_magnitudes)
+
+            if max_magnitude == 0:
+                normalized_magnitudes = binned_magnitudes
+            else:
+                normalized_magnitudes = binned_magnitudes / max_magnitude
+                
+            idx = np.argmax(normalized_magnitudes)
+            freq = frequencies[bin_edges[idx]]
+            lights.frequency_to_color(frequency=freq)
+            print(f"Bin index: {idx}, Frequency: {frequencies[bin_edges[idx]]} Hz")
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print("Exiting...")
+        lights.clear()
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+def initialize():
+    """
+    Initialize and begin stream.
+    """
+    p = pyaudio.PyAudio()
+    stream = open_stream(p)
+    lights = Lights()
+    bin_edges = np.linspace(0, CHUNK // 2, NUM_BINS + 1, dtype=int)
+    frequencies = np.linspace(0, RATE / 2, CHUNK // 2)
+    read_audio(
+        stream      = stream, 
+        frequencies = frequencies, 
+        lights      = lights,
+        bin_edges   = bin_edges,
+        p           = p
     )
-
-stream = open_stream()
-
-lights = Lights()
-
-
-bin_edges = np.linspace(0, CHUNK // 2, NUM_BINS + 1, dtype=int)
-frequencies = np.linspace(0, RATE / 2, CHUNK // 2)
-
-# print(bin_edges)
-# print(frequencies)
-
-# Continuously collect sound from audio stream and process it
-try:
-    while True:
-
-        try:
-            if not stream.is_active():
-                stream = open_stream()
-            data = np.frombuffer(stream.read(CHUNK, exception_on_overflow=False), np.float32)
-        except IOError as e:
-            # Handle buffer overflow or stream errors
-            print(f"Error reading audio stream: {e}")
-            continue
-
-        yf = fft(data)
-        magnitudes = np.abs(yf[:CHUNK // 2])
-        binned_magnitudes = [np.sum(magnitudes[bin_edges[i]:bin_edges[i + 1]]) for i in range(NUM_BINS)]
-
-        # Check if max is zero to avoid division by zero
-        max_magnitude = np.max(binned_magnitudes)
-        if max_magnitude == 0:
-            normalized_magnitudes = binned_magnitudes
-        else:
-            normalized_magnitudes = binned_magnitudes / max_magnitude
-
-        idx = np.argmax(normalized_magnitudes)
-        freq = frequencies[bin_edges[idx]]
-        lights.frequency_to_color(frequency=freq)
-
-        print(f"Bin index: {idx}, Frequency: {frequencies[bin_edges[idx]]} Hz")
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    lights.clear()
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
